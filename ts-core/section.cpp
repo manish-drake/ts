0f0d87d1dab11f84bcb44f3f3ead4a5535be890a
Manish Verma <Manish Verma>
2017-03-11 23:59:19 +0530

    completed core.section class

--- ts-core/section.cpp
+++ ts-core/section.cpp
@@ -1,6 +1,25 @@
 #include "section.h"
 
 
+int Section::id()
+{
+    return this->m_id;
+}
+
+QString Section::name()
+{
+    return this->m_name;
+}
+
+void Section::setName(QString name)
+{
+    this->m_name = name;
+}
+void Section::setId(int id)
+{
+    this->m_id = id;
+}
+
 Section::Section()
 {
 }
--- ts-core/section.h
+++ ts-core/section.h
@@ -2,12 +2,23 @@
 #define SECTION_H
 
 #include "ts-core_global.h"
+#include <QString>
 
 class TSCORESHARED_EXPORT Section
 {
 
 public:
+    int id();
+    void setId(int id);
+
+    QString name();
+    void setName(QString name);
+
     Section();
+    ~Section();
+private:
+    int m_id;
+    QString m_name;
 };
 
 #endif // SECTION_H
